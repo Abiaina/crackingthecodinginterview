631501d8743ebb59461903e0e0ffba379886aeb1
Michael Penkov <misha.penkov@gmail.com>
2015-01-19 12:26:13 +0900

    Solved problem 3.

---
+++ problem3.py
@@ -0,0 +1,23 @@
+#
+# Given a string, determine if one is a permutation of the other
+#
+
+from collections import Counter
+
+
+def is_permutation(s1, s2):
+    return Counter(s1) == Counter(s2)
+
+
+def main():
+    assert is_permutation("misha", "ahsim")
+    assert is_permutation("foobar", "raboof")
+    assert is_permutation("radar", "radar")
+
+    s1 = raw_input("Enter a string: ")
+    s2 = raw_input("Enter another string: ")
+    print "is_permutation(%s, %s):" % (repr(s1), repr(s2)),
+    print is_permutation(s1, s2)
+
+if __name__ == "__main__":
+    main()
