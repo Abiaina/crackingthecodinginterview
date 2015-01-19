4887e7258b39f9e1ffbecf2369731dc55099a71d
Michael Penkov <misha.penkov@gmail.com>
2015-01-19 13:13:23 +0900

    Solved problem 4

---
+++ problem4.py
@@ -0,0 +1,39 @@
+#
+# Write a method to replace all spaces in a string with "%20".  You may assume
+# that the string has sufficient space at the end to hold additional
+# characters. (Note: if implementing in Java, please use a character array so
+# that you can perform this operation in place)
+#
+
+SPACE = " "
+
+
+def pad(l):
+    """Pad a list with dummy elements."""
+    return l + [None] * (l.count(SPACE) * 2)
+
+
+def escape_spaces(s):
+    l = list(s)
+    length = len(l)
+    num_spaces = l.count(SPACE)
+    out = length - 1 + num_spaces * 2
+    l = pad(l)
+    for i in xrange(length - 1, -1, -1):
+        if l[i] == SPACE:
+            l[out - 2], l[out - 1], l[out] = "%", "2", "0"
+            out -= 3
+        else:
+            l[out] = l[i]
+            out -= 1
+    return "".join(l)
+
+
+def main():
+    assert escape_spaces("test") == "test"
+    assert escape_spaces("test this") == "test%20this"
+    assert escape_spaces("this is a test") == "this%20is%20a%20test"
+    assert escape_spaces("Mr John Smith") == "Mr%20John%20Smith"
+
+if __name__ == "__main__":
+    main()
