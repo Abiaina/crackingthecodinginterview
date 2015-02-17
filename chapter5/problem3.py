04304c53b309a3a1df021475d1e1149212cebd6b
Michael Penkov <misha.penkov@gmail.com>
2015-02-17 18:57:08 +0900

    Rewrote solution after reading answers

--- chapter5/problem3.py
+++ chapter5/problem3.py
@@ -7,28 +7,44 @@
 def larger(n):
     if n == 0:
         raise ValueError("number has no 1 bits")
-    mask = 0x01
-    to_add = 0x02
-    while n ^ mask == 0:
+    num_ones = 0
+    counter = 0
+    mask = 1
+    while not (num_ones and n & mask == 0):
+        counter += 1
+        if mask & n:
+            num_ones += 1
         mask <<= 1
-        to_add <<= 1
-    return n - mask + to_add
+    n |= mask
+    mask -= 1
+    n &= ~mask
+    mask >>= counter - num_ones + 1
+    return n | mask
 
 
 def smaller(n):
     tmp = n
-    mask = 0x02
-    to_add = 0x01
+    num_bits = 0
     while tmp:
+        num_bits += 1
         tmp >>= 1
+
+    num_zeros = 0
+    counter = 0
+    mask = 1
+    while not (num_zeros and n & mask):
+        if counter == num_bits:
+            raise ValueError("number can be represented entirely by 1 bits")
+        counter += 1
+        if mask & n == 0:
+            num_zeros += 1
         mask <<= 1
-        to_add <<= 1
-    while mask and n ^ mask == 0:
-        mask >>= 1
-        to_add >>= 1
-    if not mask:
-        raise ValueError("this number is already the smallest possible")
-    return n - mask + to_add
+
+    mask = (mask << 1) - 1
+    n &= ~mask
+    lsb_mask = (1 << (num_zeros - 1)) - 1
+    mask = (mask >> 1) & ~lsb_mask
+    return n | mask
 
 
 def solution(n):
--- chapter5/test3.py
+++ chapter5/test3.py
@@ -9,5 +9,18 @@ class TestSolution(unittest.TestCase):
         self.assertRaises(ValueError, problem3.solution, 1)
 
     def test_valid(self):
-        self.assertEquals(problem3.solution(6), (12, 5))
+        self.assertEquals(problem3.solution(6), (9, 5))
         self.assertEquals(problem3.solution(5), (6, 3))
+
+
+class TestLarger(unittest.TestCase):
+
+    def test(self):
+        self.assertEquals(problem3.larger(124), 143)
+
+
+class TestSmaller(unittest.TestCase):
+
+    def test(self):
+        self.assertRaises(ValueError, problem3.smaller, 0)
+        self.assertRaises(ValueError, problem3.smaller, 1)
